5300076e8b8dd4afa9ffa85898b1d5596831dd8b
leondb999 <led.1999led@gmail.com>
2021-02-22 20:47:14 +0100

    Initial Commit

--- app/src/main/java/com/example/saufundbraus/MainActivity.java
+++ app/src/main/java/com/example/saufundbraus/MainActivity.java
@@ -12,45 +12,40 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button_add_player;
+    Button button_start_game;
+    EditText edittext_player_name;
+    ListView listview_player_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button_add_player = (Button)findViewById(R.id.button_add_player);
+        button_start_game = (Button)findViewById(R.id.button_start_game);
+        edittext_player_name = (EditText)findViewById(R.id.edittext_player_name);
+        listview_player_list = (ListView)findViewById(R.id.listview_player_list);
+        List<String> player_name = new ArrayList<String>();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,player_name);
+        listview_player_list.setAdapter(arrayAdapter);
+        button_add_player.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                player_name.add(edittext_player_name.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+                edittext_player_name.setText("");
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
