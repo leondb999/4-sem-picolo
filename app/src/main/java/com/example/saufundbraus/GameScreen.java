7acd67f3ceb7071a7bd1bddec2fad5cb6e81e836
leondb999 <led.1999led@gmail.com>
2021-03-02 14:52:12 +0100

    Fragen screen

--- app/src/main/java/com/example/saufundbraus/GameScreen.java
+++ app/src/main/java/com/example/saufundbraus/GameScreen.java
@@ -3,25 +3,56 @@ package com.example.saufundbraus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GameScreen extends AppCompatActivity {
-
+    Button exit;
+    TextView frage_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
-        /*
+        exit = (Button)findViewById(R.id.button_exit);
+        frage_view = (TextView)findViewById(R.id.textFrage);
+
         ArrayList<String> spielerliste = (ArrayList<String>) getIntent().getStringArrayListExtra("spielerliste");
-        String text = spielerliste.get(1);
-        int duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(this, text, duration);
+        String s1 = spielerliste.get(0);
+        ArrayList<String> fragenliste = new ArrayList<String>();
+        fragenliste.add("Pagnia kann mich mal");
+        fragenliste.add(" gewinne das Spiel asjdaisdjlasjdlkasjdlajsdljasöldkjasldjalsjdlasjdlakjsdölasjdlasjdl asjd ölkasjaslkdj asöldj aslkdja slkdj askd jölsa jdlias jd asöldjasljdlas jd");
+        ArrayList<String> fragenliste_finished = new ArrayList<String>();
+        int counter_spieler = 1;
 
-        toast.show();
-         */
+        frage_view.setText(s1 + fragenliste.get(0));
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        /*
+        frage_view.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(liste zuende){
+                    navigate to screen --> Spiel zuende;
+                } else {
+                    erhöre globale int variable
+                }
+
+            }
+
+        });
+*/
 
     }
 }
--- app/src/main/java/com/example/saufundbraus/MainActivity.java
+++ app/src/main/java/com/example/saufundbraus/MainActivity.java
@@ -2,17 +2,8 @@ package com.example.saufundbraus;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,14 +38,14 @@ public class MainActivity extends AppCompatActivity {
                 edittext_player_name.setText("");
             }
         });
-/*
+
         button_start_game.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, GameScreen.class);
-             //   i.putStringArrayListExtra("spielerliste", player_name);
+                i.putStringArrayListExtra("spielerliste", player_name);
                 startActivity(i);
             }
-        });*/
+        });
     }
 }
