10834544e839bc2d7f17b1cfeefcebfd86970b64
Jan <alexanderheller2000@gmail.com>
2021-03-08 13:52:43 +0100

    Info coding

--- app/src/main/java/com/example/saufundbraus/Info.java
+++ app/src/main/java/com/example/saufundbraus/Info.java
@@ -3,12 +3,26 @@ package com.example.saufundbraus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Info extends AppCompatActivity {
+
+    Button button_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        button_back = (Button) findViewById(R.id.button_back);
+
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
